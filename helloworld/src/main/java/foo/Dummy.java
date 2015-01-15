dbf08ffcfbf695f90aaa135e1ab9b7f86eac3809
srbhlz <sandra.rebholz@gmx.de>
2015-01-15 15:55:51 +0100

    added attribute anzahl and initialization

--- helloworld/src/main/java/foo/Dummy.java
+++ helloworld/src/main/java/foo/Dummy.java
@@ -4,15 +4,18 @@ public class Dummy {
 	
 	private String localAttribut;
 	private int onlineAttribut;
+	private int anzahl;
 
-	public Dummy() {
+	public Dummy(int dieAnzahl) {
 		// TODO Auto-generated constructor stub
+		this.anzahl = dieAnzahl;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
 	
 	public String getLocalAttribut() {
 		return this.localAttribut;
