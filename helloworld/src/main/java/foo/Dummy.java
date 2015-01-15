c158e83487802d5d12c8c88524190fcf01093b06
srbhlz <sandra.rebholz@gmx.de>
2015-01-15 15:15:49 +0100

    Added attribute onlineAttribut

--- helloworld/src/main/java/foo/Dummy.java
+++ helloworld/src/main/java/foo/Dummy.java
@@ -1,6 +1,8 @@
 package foo;
 
 public class Dummy {
+	
+	private int onlineAttribut;
 
 	public Dummy() {
 		// TODO Auto-generated constructor stub
@@ -9,6 +11,10 @@ public class Dummy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public int getOnlineAttribut() {
+		return this.onlineAttribut;
 	}
 
 }
