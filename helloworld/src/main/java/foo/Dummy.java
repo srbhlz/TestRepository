1f6e814e5781251b364f9cd76503c531e3ac827f
srbhlz <sandra.rebholz@gmx.de>
2015-01-15 15:18:40 +0100

    added attribute localAttribut

--- helloworld/src/main/java/foo/Dummy.java
+++ helloworld/src/main/java/foo/Dummy.java
@@ -1,6 +1,8 @@
 package foo;
 
 public class Dummy {
+	
+	private String localAttribut;
 
 	public Dummy() {
 		// TODO Auto-generated constructor stub
@@ -9,6 +11,10 @@ public class Dummy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public String getLocalAttribut() {
+		return this.localAttribut;
 	}
 
 }
