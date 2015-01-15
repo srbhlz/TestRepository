4ac9dde7ec36479546d659cd0ff7f944b7495706
srbhlz <sandra.rebholz@gmx.de>
2015-01-15 15:15:00 +0100

    Created same class again

---
+++ helloworld/src/main/java/foo/Dummy.java
@@ -0,0 +1,14 @@
+package foo;
+
+public class Dummy {
+
+	public Dummy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
